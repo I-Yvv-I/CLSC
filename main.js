711450881c2c729fa2cad224f87b473a76410646
Yvv <131615792+I-Yvv-I@users.noreply.github.com>
2025-03-29 19:30:50 -0400

    error message handle

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 function showText() {
     let text = document.getElementById("sc").value;
     let output = lex(text)
-    document.getElementById("output").innerText = output.toString();
+    if (typeof(output) == "string") {
+        console.log(output);
+        document.getElementById("output").innerText = output;
+        return;
+    }
+    document.getElementById("output").innerText = output;
 }
