bac397681b575f6de1e9a31706c37340ee3c71ad
Yvv <131615792+I-Yvv-I@users.noreply.github.com>
2025-03-29 18:47:46 -0400

    added things?

--- lexer.js
+++ lexer.js
@@ -1,3 +1,23 @@
+const keywords = ["ft", "lt", "rl"]
+
 function lex(script) {
-    console.log(script)
+    let fl = script.split("\n");
+    tokens = [];
+
+    let tok = ""
+
+    for (let line of fl) {
+        for (let char of line.split("")) {
+            tok = tok + char;
+            console.log(tok);
+            if (keywords.includes(tok)) {
+                tokens.push("KEY:" + tok)
+                tok = ""
+            }
+        }
+        tok = ""
+    }
+
+    console.log(tokens);
+    return tokens;
 }
