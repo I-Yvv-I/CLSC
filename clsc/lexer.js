53ae3b416a4dc47d174a613eb8896d1fffb5958e
Yvv <131615792+I-Yvv-I@users.noreply.github.com>
2025-03-29 21:52:22 -0400

    Update lexer.js

--- clsc/lexer.js
+++ clsc/lexer.js
@@ -1,51 +1,64 @@
-const keywords = ["feature", "letter", "rule"]
-const symbols = ["=","{","}","[","]"]
+const keywords = ["feature", "letter", "rule"];
 
 function lex(script) {
     let fl = script.split("\n");
     let tokens = [];
 
-    let tok = "";
-    let type = ""
+    let inrule = false;
 
     let i = 0;
     for (let line of fl) {
-        i += 1;
         let j = 0;
-        for (let char of line.split("")) {
-            j += 1;
-            tok = tok + char;
-            //console.log(tok);
-            if (char == ' ') {
-                if (type == "" && keywords.includes(tok.slice(0, -1))) {
-                    tokens.push("KEY:" + tok.slice(0, -1));
-                    type = tok.slice(0, -1)
-                } else if (type == "feature") {
-                    if (tok.slice(0, -1) == "=") {
-                        tokens.push("VLS:" + line.slice(j));
-                        type = ""
-                        continue;
-                    } else if (tok.slice(0, -1).length < 2) {
-                        return "Line " + i + ", Character " + j + ": '" + tok.slice(0, -1) + "' must be more then one character long.";
-                    } else {
-                        tokens.push("VAR:" + tok.slice(0, -1));
-                    }
-                } else if (type = "letter") {
-                    if (tok.slice(0, -1) == "=") {
-                        tokens.push("VLS:" + line.slice(j));
-                        type = ""
-                        continue;
-                    } else {
-                        tokens.push("LET:" + tok.slice(0, -1));
-                    }
+        let type = "";
+        for (let char of line.split(" ")) {
+            console.log(type,char,line.split(" "));
+            if (type == "" && keywords.includes(char)) {
+                tokens.push("KEY:" + char);
+                type = char;
+                if (type == "rule") {
+                    inrule = true;
                 }
-                tok = "";
+            } else if (type == "" && inrule == true) { // In-Rule
+                
+            } else if (type == "feature") { // Feature
+                if (char == "=") {
+                    if (tokens[tokens.length-1].slice(0,3) != "VAR") {
+                        return "Line " + i + ": '" + char + "' cannot be there, you need a variable dummy.";
+                    }
+                    tokens.push("VLS:" + line.split(" ")[j+1]);
+                    break;
+                } else {
+                    tokens.push("VAR:" + char);
+                }
+            } else if (type == "letter") { // Letter
+                if (char == "=") {
+                    if (tokens[tokens.length-1].slice(0,3) != "LET") {
+                        return "Line " + i + ": '" + char + "' cannot be there, you need a letter dummy.";
+                    }
+                    tokens.push("VLS:" + line.split(" ")[j+1]);
+                    break;
+                } else {
+                    tokens.push("LET:" + char);
+                }
+            } else if (type == "rule") { // Rule
+                if (char == "=") {
+                    if (tokens[tokens.length-1].slice(0,3) != "RUL") {
+                        return "Line " + i + ": '" + char + "' cannot be there, you need a rule-name dummy.";
+                    }
+                    if (line.split(" ")[j+1] != "{") {
+                        return "Line " + i + ": Wheres the open curly bracket?";
+                    }
+                    break;
+                } else {
+                    tokens.push("RUL:" + char);
+                }
             }
+            j += 1;
         }
-        tok = "";
-        console.log(tokens);
+        console.log(tokens.join(","));
+        i += 1;
     }
-//mewo
-    console.log(tokens);
+
+    console.log(tokens.join(","));
     return tokens;
 }
